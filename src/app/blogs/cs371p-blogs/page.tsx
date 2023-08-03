b38723fcd446fbc109604e85eab121a7108777b2
Forrest Sun <forrest.sun@auctane.com>
2023-08-03 16:31:38 -0500

    Added react-select for class blogs dropdown and begin CS371p transfer

---
+++ src/app/blogs/cs371p-blogs/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import BlogPostHeader from "@/components/blog/blog-post-header";
+import styles from "./page.module.css";
+import { ScrollTopButton } from "@/components/scroll-top/scroll-top";
+import { Fragment } from "react";
+import * as WK1 from "./week1";
+
+const blogPosts = [WK1];
+
+export default function CS371pBlogs() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.main}>
+        <ScrollTopButton />
+        {blogPosts.map((blogPost) => (
+          <Fragment key={blogPost.INDEX}>
+            <BlogPostHeader index={blogPost.INDEX} date={blogPost.DATE} />
+            {blogPost.QUESTIONS.map((question, index) => (
+              <Fragment key={index}>
+                <strong>{question}</strong>
+                <i>{blogPost.ANSWERS[index]}</i>
+              </Fragment>
+            ))}
+            <hr />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,57 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.css";
+import Select from "react-select";
 
 export const Header = () => {
+  const [selectedBlogOption, setSelectedBlogOption] = useState<string>();
+
+  const blogOptions = [
+    { value: "cs373-blogs", label: "CS 373 Blogs" },
+    { value: "cs371p-blogs", label: "CS 371p Blogs" },
+  ];
+
   return (
     <div className={styles.header}>
       {"FORREST SUN"}
       <div className={styles.headerMenu}>
         <button onClick={() => (window.location.href = "/")}>{"HOME"}</button>
-        <button onClick={() => (window.location.href = "/blogs/cs373-blogs")}>
-          {"CLASS BLOGS"}
-        </button>
+        <Select
+          components={{
+            DropdownIndicator: () => null,
+            IndicatorSeparator: () => null,
+          }}
+          controlShouldRenderValue={false}
+          onChange={(selectedOption) => {
+            window.location.href = `/blogs/${selectedOption?.value}`;
+          }}
+          options={blogOptions}
+          placeholder={"CLASS BLOGS"}
+          styles={{
+            control: (provided) => ({
+              ...provided,
+              border: "none",
+              boxShadow: "none",
+              backgroundColor: "transparent",
+            }),
+            menu: (provided) => ({
+              ...provided,
+              backgroundColor: "black",
+              color: "white",
+            }),
+            option: (provided, state) => ({
+              ...provided,
+              backgroundColor: state.isFocused ? "#1f1f1f" : "black",
+              fontSize: "16px",
+              color: "white",
+            }),
+            placeholder: (provided) => ({
+              ...provided,
+              color: "white",
+            }),
+          }}
+        />
         <button onClick={() => (window.location.href = "/about")}>{"ABOUT"}</button>
       </div>
     </div>
