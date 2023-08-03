41974677e251b1390f57e7c68532f5adc15ba8c5
Forrest Sun <forrest.sun@auctane.com>
2023-08-03 11:02:58 -0500

    Added mobile support for Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import main from "../../public/main.jpg";
 
 export default function Home() {
   return (
-    <div className={styles.main}>
-      <h2>Home Page</h2>
-      <div>Hello, world!</div>
-      <div>I am Forrest Sun.</div>
-      <Image alt="Main Picture" src="/main.JPG" quality={100} width={500} height={500} />
+    <div className={styles.page}>
+      <div className={styles.main}>
+        <h2>Home Page</h2>
+        <div>Hello, world!</div>
+        <div>I am Forrest Sun.</div>
+        <div className={styles.imgContainer}>
+          <Image alt="Main Picture" src={main} fill />
+        </div>
+      </div>
     </div>
   );
 }
