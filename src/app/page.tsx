279b0e82e5fe6fe40bdfa7675f188482b6bb2532
Forrest Sun <forrest.sun@auctane.com>
2023-08-09 11:08:40 -0500

    Minor update to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
+  const utcsLink = <a href="https://www.cs.utexas.edu/">{"Computer Science"}</a>;
+  const damLink = <a href="https://ugs.utexas.edu/bdp/programs/dam">{"Digital Arts & Media"}</a>;
+
   return (
     <div className={styles.page}>
       <div className={styles.main}>
-        <h2>Home Page</h2>
-        <div>Hello, world!</div>
-        <div>I am Forrest Sun.</div>
         <div className={styles.imgContainer}>
-          <Image alt="Main Picture" src={"/main.JPG"} fill />
+          <Image alt="Main Picture" src={"/main.jpeg"} fill objectFit="contain" />
+        </div>
+        <div className={styles.text}>
+          {"I'm from Houston, TX and I am a"} {utcsLink}
+          {" major at UT Austin getting a certificate in "} {damLink}
         </div>
       </div>
     </div>
